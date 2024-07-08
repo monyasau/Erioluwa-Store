b1d73f4c4121db82ec88554df9903750e457f391
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-08 17:39:55 +0100

    Completed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,37 @@ const Footer = () => {
     return (
         <div className="w-full bg-[#121212] py-16">
 
-            <div className="max-w-screen-xl mx-auto grid grid-cols-2 justify-center items-center">
+            <div className="max-w-screen-xl mx-auto grid grid-cols-2 capitalize justify-center items-center">
                 <div>
                     <h1 className="text-4xl font-trajan text-white">
                         Stay Updated on Latest Product Releases
                     </h1>
-                    <div>
-                        <input type="email" className="capitalize" placeholder="Enter your email"/>
-                        <button className="bg-white py-4 px-8 rounded-full">Subscribe</button>
+                    <div className="flex font-trajan gap-4 py-16 text-white/80">
+                        <input type="email" className="capitalize bg-[#292929] text-white px-8 py-4 rounded-full w-[70%]" placeholder="ENTER YOUR EMAIL" />
+                        <button className=" bg-[#292929] py-4 px-10 rounded-full">SUBSCRIBE</button>
                     </div>
                 </div>
-                <div></div>
+                <div className="flex justify-between pl-[50%] font-trajan h-full">
+                <div className=" text-white/90">
+                        <h1 className="pb-8 text-xl">PAGES</h1>
+                        <div className="flex flex-col text-white/80 ">
+                        <a href="#">About Us</a>
+                        <a href="#">Catalogue</a>
+                        <a href="#">Testimonials</a>
+                        <a href="#">Articles & Blogs</a>
+                        </div>
+                    </div>
+                    <div className=" text-white/90">
+                        <h1 className="pb-8 text-xl">Social Media</h1>
+                        <div className="flex flex-col text-white/80">
+                        <a href="#">Linkedin</a>
+                        <a href="#">Twitter</a>
+                        <a href="#">Instagram</a>
+                        <a href="#">Facebook</a>
+                        <a href="#">Pinterest</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
