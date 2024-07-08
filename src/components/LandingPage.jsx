f637e2ef195a3b8ca3c497b4799d2de6fe7999fd
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-08 17:22:16 +0100

    Fixed ui

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,22 +9,22 @@ const LandingPage = () => {
             <div className="w-full " style={{background: "linear-gradient(180deg, #FCFCFC 0%, #EAEAEA 100%)"}}>
                 <div className="max-w-screen-xl mx-auto h-screen rounded-3xl items-center flex p-20" style={{ background: "url(" + mainImage + ")", backgroundSize: "cover" }}>
                     <div className="h-fit w-[80%]">
-                        <h1 className="text-6xl font-trajan text-white drop-shadow-xl">
+                        <h1 className="text-6xl font-trajan text-white ">
                             Discover Timeless & Elegant Luxury Men's Brogues
                         </h1>
-                        <div className="">
+                        <div className="font-bold font-cabinet">
 
                             <p className="text-white w-[45%] py-12 text-xl">
                                 Our exclusive collection of executive men’s brogues, crafted to elevate your style effortlessly.
                             </p>
-                            <div className="flex gap-4">
+                            <div className="flex   gap-4">
                                 <button className="bg-white py-4 px-8 rounded-full">Browse products</button>
-                                <button className="border-white border py-4 px-8 rounded-full">About us</button>
+                                <button className="border-white border text-white py-4 px-8 rounded-full">About us</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="max-w-screen-xl mx-auto  my-16">
+                <div className="max-w-screen-xl mx-auto  mt-16">
                     <h1 className="text-[80px] font-trajan">
                         featured products
                     </h1>
