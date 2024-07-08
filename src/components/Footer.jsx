6aa8e1f49d30ac5c1bc7cca85a4f0b1446cf3450
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-08 19:07:07 +0100

    fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
                         Stay Updated on Latest Product Releases
                     </h1>
                     <div className="md:flex font-trajan gap-4 py-16 text-white/80">
-                        <input type="email" className="capitalize bg-[#292929] text-white px-8 py-4 mb-4 rounded-full md:w-[70%]" placeholder="ENTER YOUR EMAIL" />
-                        <button className=" bg-[#292929] py-4 px-10 rounded-full">SUBSCRIBE</button>
+                        <input type="email" className="capitalize  w-full bg-[#292929] text-white px-8 py-4 mb-4 rounded-full md:w-[70%]" placeholder="ENTER YOUR EMAIL" />
+                        <button className=" bg-[#292929] w-full md:w-fit py-4 px-10 rounded-full">SUBSCRIBE</button>
                     </div>
                 </div>
-                <div className="flex-col md:flex-row flex justify-between mx-auto md:m-0 md:pl-[50%] font-trajan h-full">
+                <div className="gap-x-4 md:gap-0 flex-row flex justify-between mx-auto md:m-0 md:pl-[50%] font-trajan h-full">
                 <div className=" text-white/90">
                         <h1 className="pb-8 text-xl">PAGES</h1>
                         <div className="flex flex-col text-white/80 ">
