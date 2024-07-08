a111eee2e9178df45cf798b5402ebd66979ac170
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-08 18:09:48 +0100

    Fixed responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 const Footer = () => {
     return (
-        <div className="w-full bg-[#121212] py-16">
+        <div className="w-full px-2 md:px-0 bg-[#121212] py-16">
 
-            <div className="max-w-screen-xl mx-auto grid grid-cols-2 capitalize justify-center items-center">
+            <div className="max-w-screen-xl mx-auto grid md:grid-cols-2 capitalize justify-center items-center">
                 <div>
                     <h1 className="text-4xl font-trajan text-white">
                         Stay Updated on Latest Product Releases
                     </h1>
-                    <div className="flex font-trajan gap-4 py-16 text-white/80">
-                        <input type="email" className="capitalize bg-[#292929] text-white px-8 py-4 rounded-full w-[70%]" placeholder="ENTER YOUR EMAIL" />
+                    <div className="md:flex font-trajan gap-4 py-16 text-white/80">
+                        <input type="email" className="capitalize bg-[#292929] text-white px-8 py-4 mb-4 rounded-full md:w-[70%]" placeholder="ENTER YOUR EMAIL" />
                         <button className=" bg-[#292929] py-4 px-10 rounded-full">SUBSCRIBE</button>
                     </div>
                 </div>
-                <div className="flex justify-between pl-[50%] font-trajan h-full">
+                <div className="flex-col md:flex-row flex justify-between mx-auto md:m-0 md:pl-[50%] font-trajan h-full">
                 <div className=" text-white/90">
                         <h1 className="pb-8 text-xl">PAGES</h1>
                         <div className="flex flex-col text-white/80 ">
