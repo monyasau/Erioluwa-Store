04c18ea0329320bf59661068d5819bbc4817cb29
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-08 10:39:58 +0100

    Changed icon

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 
 const TopNav = () => {
-const [dropDownActive, setDropDownActive] = useState(false)
+    const [dropDownActive, setDropDownActive] = useState(false)
 
     return (
         <div className="w-full ">
 
             <div className="max-w-screen-xl text-primary py-16 px-6 md:px-0 mx-auto flex justify-between items-center">
                 <h1 className="text-[30px] font-bold">
-                Oxford Brogues
+                    Oxford Brogues
                 </h1>
                 <div className="md:flex gap-16 items-center font-bold text-[20px] hidden">
                     <a href="#">Home</a>
@@ -16,11 +16,10 @@ const [dropDownActive, setDropDownActive] = useState(false)
                     <a href="#">Support</a>
                 </div>
                 <div className="md:hidden" aria-label='dropdown button'>
-                    <svg className="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4 5H14" stroke="#005DA1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M4 12H20" stroke="#005DA1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M4 19H20" stroke="#005DA1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                    <svg width="40" className="w-8 h-8"  height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11 20H29M11 14H29M11 26H29" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
+
                 </div>
                 <div className='absolute  backdrop-blur-sm md:hidden w-full h-full top-0 left-0'>
                     dnnddn
