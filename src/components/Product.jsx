d179be794ff7c983a6ce1c730d0ccd32381ec2c4
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-13 08:38:16 +0100

    Fixed product rendering

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
-const Product = ({ name, price, id, image, setCart, cart }) => {
+const Product = ({ productData, setCart, cart }) => {
+    let id = productData.id;
     const [inCart, setInCart] = useState(cart.includes(id));
 
     useEffect(() => {
@@ -19,17 +20,22 @@ const Product = ({ name, price, id, image, setCart, cart }) => {
 
     return (
         <div className="relative">
-            <div className="absolute right-2 top-2 px-4 py-2 rounded-full text-white font-cabinet bg-black/50 w-fit">
+            <div className="absolute right-2 top-2 rounded-full text-white font-cabinet bg-black/50 w-fit" >
                 {inCart ? (
-                    <button onClick={removeFromCart} className="text-red-100">Remove from cart</button>
+                    <button onClick={removeFromCart} className=" px-4 py-2 text-red-100">Remove from cart</button>
                 ) : (
-                    <button onClick={addToCart}>Add to cart</button>
+                    <button onClick={addToCart} className=" px-4 py-2">Add to cart</button>
                 )}
             </div>
-            <img src={image} className="rounded-2xl md:h-[420px]" alt="" />
+            {productData.photos && productData.photos[0] ?
+            (
+                <img src={`https://api.timbu.cloud/images/${productData.photos[0].url}`} className="  border rounded-2xl md:h-[420px]" alt="" />
+            ):
+            null
+            }
             <div aria-label="info" className="flex text-2xl justify-between my-4">
-                <h1 className="font-trajan capitalize">{name}</h1>
-                <p className="font-cabinet text-[#787878]">₦{price}</p>
+                <h1 className="font-trajan capitalize">{productData.name}</h1>
+                <p className="font-cabinet text-[#787878]">₦{productData.current_price[0].NGN}</p>
             </div>
         </div>
     );
